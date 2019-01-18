7f8411b4b7c296d1c4197104e9b9e0e068290109
s3731790 <45913842+s3731790@users.noreply.github.com>
2019-01-18 23:34:55 +1100

    Update index.php

--- a2/index.php
+++ a2/index.php
@@ -14,27 +14,281 @@
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+     <!-- Image sourced from www.rmit.edu.au for educational purposes only -->
+      <div> <img src='../media/lunardoLogo4.png' alt='Lunardo logo' height=125
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <div>  href="#">ABOUT US</a> |
+  <a href="#">SEATS AND PRICES</a> |
+  <a href="#">NOW SHOWING</a> |
+  <a href="#">BOOKING</a> |</div>
     </nav>
 
     <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
+    <!---------------  Display ABOUT US -------------->
+     <p>  </p> <hr>
+     <div class="section">
+      <h2> <span style="color:#00308f;"> ABOUT US </span>  </h2>
+  <h3> 
+                 
+    Lunardo has extensive improvements and renovations for you to enjoy your favourite movies.
+    </p>    
+Lunardo offers reclinable first class seats and standard seats providing relaxing and much comfort seats for you to enjoy watching.
+           
+    <p>    
+Lunardo is equipped with the latest Dolby projection and sound systems - upgraded with 3D Dolby Vision projection and Dolby Atmos sound.
+
+    </p>
+Lunardo cinema where the show starts with you around...
+    <p>
+See you soon!
+    </p>
+     </h3>
+<!----------------- Display SEATS AND PRICES ------------->
+    <p>  <hr
+    <div class="section">
+    <h2> <span style="color:#00308f;"> SEATS AND PRICES </span>  </h2>
+    <div><img src='../media/lunardoSeats1.jpg' alt='First Class Seats' height=500 Standard Seats
+    <div><img src='../media/lunardoSeats1A.jpg' alt='First Class Seats Accesories' 
+    height=500 First Class Seats Accessories
+    <div><img src='../media/lunardoSeatsStd.jpg' alt='Standard Seats' 
+    height=500 Standard Class Seats 
+    <div><img src='../media/lunardoSeatsStdA.jpg' alt='Standard Seats Accesories' 
+    height=500 Standard Class Seats Accessories  
+    </div>
+  
+   </p>  
+    <div class="section">
+                 
+     <h3> <span style="color:#00308f;"> SEAT PRICES (A$)</span>  </h3>  
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;
+}
+</style>
+<body>
+
+<table>
+  <tr>
+    <th>Seat Type</th>
+    <th>Seat Code</th>
+    <th>Mon and Wed AND 12pm on weekdays</th>
+    <th>All other times</th>  
+  </tr>
+  <tr>
+    <td>Standard Adult</td>
+    <td>STA</td>
+    <td>14.00</td>
+    <td>19.80</td>  
+  </tr>
+  <tr>
+    <td>Standard Concession</td>
+    <td>STP</td>
+    <td>12.50</td>
+    <td>17.50</td>  
+  </tr>
+  <tr>
+    <td>Standard Child</td>
+    <td>STC</td>  
+    <td>11.00</td>
+    <td>15.30</td>  
+  </tr>
+  <tr>
+    <td>First Class Adult</td>
+    <td>FCA</td>  
+    <td>24.00</td>
+    <td>30.00</td> 
+  </tr>
+  <tr>
+    <td>First Class Concession</td>
+    <td>FCP</td>  
+    <td>22.50</td>
+    <td>27.00</td>
+  </tr>
+  <tr>
+    <td>First Class Child</td>
+    <td>FCC</td>  
+    <td>21.00</td>
+    <td>24.00</td>
+ </tr>
+</table>
+ <br style="margin-bottom:240px;"/>  
+    
+<!----------------- Display NOW SHOWING posters ------------------->
+      <hr>
+    <div class="section">
+     <h2> <span style="color:#00308f;"> NOW SHOWING </span>  </h2>   
+     <div><img src='../media/theGirlInTheSpidersWeb2.jpg' alt='The Girl in the Spider Web' height=500 The Girl in the Spider Web
+              
+    <div><img 
+    src='../media/aStarIsBorn2.jpg' 
+    alt='A Star is Born' 
+    height=500 A Star is Born 
+        
+    <div><img src='../media/ralphBreaksTheInternet2.jpg' alt='Ralph Breaks the Internet' 
+    height=500 Ralph Breaks the Internet   
+        
+    <div><img 
+    src='../media/boyErased2.jpg' 
+    alt='Boy Erased' 
+    height=500 Boy Erased      
+    </div>
+
+
+<!-- Trailers 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- The Girl in the Spiders Web -->   
+    <a href='../media/theGirlInTheSpidersWeb.mp4' The Girl in the Spiders Web </a>    
+    <div><img src='../media/theGirlInTheSpidersWebTrailer.jpg' alt='The Girl in the Spider Web'  height=400 width=1000 The Girl in the Spider Web 
+    class="container"> 
+        <p>
+    <b>Make a Booking:</b>
+    <button class="Girlbtn1">Wed-Fri 9pm</button>
+    <button class="Girlbtn2">Sat-Sun 6pm</button>
+         </div> <br 
+        </p>
+              
+  <!-- A Star is Born -->   
+    <a href='../media/aStarIsBorn.mp4' A Star is Born </a>   
+    <div><img src='../media/AStarIsBornTrailer.jpg' alt='A Star is Born' height=400 width=1000 A Star is Born
+     class="container">
+        <p>
+     <b>Make a Booking:</b>
+    <button class="starBtn1">Mon-Tue 6pm</button>
+    <button class="starBtn2">Sat-Sun 3pm</button>
+    </div> <br     
+        </p>      
+
+  <!-- Ralph Breaks the Internet --> 
+    <a href='../media/ralphBreaksTheInternet.mp4' Ralph Breaks the Internet </a> 
+   <!--  <h3 class="center-align header-text  no-margin">Ralph Breaks the Internet</h3> --> 
+    <div><img src='../media/RalphBreaksTheInternetTrailer.jpg' alt='Ralph Breaks the Internet' height=400 width=1000 Ralph Breaks the Internet
+    class="container">
+        <p>
+    <b>Make a Booking:<b>
+    <button class="ralphBtn1">Mon-Tue 12pm</button>
+    <button class="ralphBtn2">Wed-Fri 6pm</button>
+    <button class="ralphBtn3">Sat-Sun 12pm</button>
+       </div> <br
+                  </p>          
+       
+  <!-- Boy Erased --->
+    <a href='../media/boyErased.mp4' Boy Erased </a> 
+      <div><img src='../media/BoyErasedTrailer.jpg' alt='Boy Erased' height=400 width=1000 Boy Erased  class="container">
+        <p>
+    <b>Make a Booking:</b>
+    <button class="boyBtn1">Wed-Fri 12pm</button>
+    <button class="boyBtn2">Sat-Sun 9pm</button>
+   </div> <br </p> <a  </a>   
+        
+<!----------------- BOOKING ------------->
+  <p>  </p>  <p>  </p> <hr>
+    <div class="section">
+                 
+     <h2> <span style="color:#00308f;"> BOOKING </span>  </h2>   
+ 
+  <body>
+  
+   <div align="left"> 
+  
+    <h3>Movie:  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   Date/Time:  </h3>
+    <form>
+   <fieldset>
+        <legend>Standard</legend>
+          <p>
+              <label for="Adult">Adult</label>
+              <input type="number" id="stdAdult" Adult="stdAdult" />
+              
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <label for="name">Name:</label>
+              <input type="text" id="name" Name="customerName" />
+              
+               <p>
+                <label for="Concession">Concession</label>
+              <input type="number" id="stdConcession" Adult="stdConcesssion" />
+            
+             &nbsp;&nbsp;&nbsp;&nbsp; &nbsp; 
+             <label for="email">E-mail:</label>
+             <input type="text" id="email" name="customerEmail" />
+              <p>
+             
+              <label for="Children">Children</label>
+              <input type="number" id="stdChildren" Adult="stdChildren" />
+             
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <label for="mobile">Mobile:</label>
+            <input type="tel" id="customerMobile" name="customerMobile" />
+            <p>
+                
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             <label for="creditCard">Credit Card:</label>
+             <input type="text" id="creditCard" name="creditCard" />
+             <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <label for="cardExpiryDate">Expiry Date:</label>
+            <input type="date" id="cardExpiryDate" name="cardExpiryDate" />  
+    
+         </fieldset>
+        
+     <fieldset>
+              
+          <legend>First Class</legend>
+          <p>
+              <label for="Adult">Adult</label>
+              <input type="number" id="firstdAdult" Adult="firstAdult" />
+               <p>
+                <label for="Concession">Concession</label>
+              <input type="number" id="firstConcession" Adult="firstConcesssion" />
+               <p>
+              <label for="Children">Children</label>
+              <input type="number" id="firstChildren" Adult="firstChildren" />
+               <p>
+                       
+         </p>
+         
+         Total $
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <button type="order" form="form" value="order">Order</button>
+ </fieldset>
+</form>     
+
+       <!-- Send input data  -->
+      <form action="https://titan.csit.rmit.edu.au/~e54061/wp/lunardo-formtest.php">
+     
     </main>
 
     <footer>
-      <div>&copy;<script>
+       <footer>
+        <p>  </p> <hr>
+        
+      &copy; Lunardo Cinema
+      <script>
+        document.write(new Date().getFullYear());  
+      </script>
+           <br>
+             757 New Avenue Castle Hill NSW Sydney  
+             &nbsp;&nbsp;Email: lunardoCinema@gmail.com 
+             &nbsp;&nbsp;&nbsp;&nbsp; Phone 1300 777 888 <br>
+       <p>   <p>  </p> 
+      Kathryn Tolentino s3731790  &nbsp; GIT link
+      <script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+      </script>
     </footer>
-
+        
   </body>
 </html>
